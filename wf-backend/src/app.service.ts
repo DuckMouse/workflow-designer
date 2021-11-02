e1b7ec7c0d4f411bd059c4b0f8d0a20e92a167ff
Auret Swanepoel <auret.swanepoel@practiceevolve.com.au>
2021-11-02 12:28:16 +1100

    basic css

--- wf-backend/src/app.service.ts
+++ wf-backend/src/app.service.ts
@@ -3,12 +3,7 @@ import { data } from './db/workflow.json';
 
 @Injectable()
 export class AppService {
-
-
   getMockedWorkflow() {
     return data;
-  }
-  getHello(): string {
-    return 'Hello World!';
   }
 }
